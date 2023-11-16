cb0cfbd931b92c403447dc11d2f0004314b337b5
Singireddy Bharath <singireddybharath99@gmail.com>
2023-11-16 22:21:41 +0530

    updated all the logic

--- todo-app/src/components/AllTodos/AllTodos.jsx
+++ todo-app/src/components/AllTodos/AllTodos.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import "./AllTodos.css";
 
-const AllTodos = ({ todoList }) => {
+const AllTodos = ({ todoList, deleteTodo, updateTodo }) => {
   return (
     <>
-      {todoList.map((todo) => {
-        return <div key={todo.id}>{todo.taskName}</div>;
-      })}
+     {todoList.map(({ id, taskName, status }) => (
+       <div className="todo-item" key={id}>
+         <div className="taskName-container">
+           <FontAwesomeIcon icon={faAngleRight} />
+           <span className="task-name">{taskName}</span>
+         </div>
+         <div className="button-container">
+           {status === "todo" ? (
+             <button className="mark-done" onClick={() => updateTodo(id)}>
+               Mark as done
+             </button>
+           ) : (
+             <button className="mark-todo" onClick={() => updateTodo(id)}>
+               Mark as Todo
+             </button>
+           )}
+           <button className="del-todo" onClick={() => deleteTodo(id)}>
+             <FontAwesomeIcon icon={faTrash} />
+           </button>
+         </div>
+       </div>
+     ))}     
     </>
   );
 };
--- todo-app/src/components/Home/home.jsx
+++ todo-app/src/components/Home/home.jsx
@@ -2,42 +2,89 @@ import React, { useState } from "react";
 import InputContainer from "../InputContainer/InputContainer";
 import { v4 as uuidv4 } from "uuid";
 import { Tab, Tabs } from "react-bootstrap";
-import CompletedTodos from "../CompletedTodos/CompletedTodos";
 import AllTodos from "../AllTodos/AllTodos";
-
 import "./home.css";
-import Todos from "../Todos/Todos";
+
 const Home = () => {
+  const [activeTab, setActiveTab] = useState("allTodos");
   const [todoList, setTodoList] = useState([]);
+  const todoArray = todoList.filter((todoItem) => todoItem.status === "todo");
+  const completedTodoList = todoList.filter(
+    (todoItem) => todoItem.status !== "todo"
+  );
+  console.log("todo list ", todoList);
+  console.log("active tab", activeTab);
   const addTodo = (inputTask) => {
     if (inputTask !== "") {
+      const capitalizedTask =
+        inputTask.charAt(0).toUpperCase() + inputTask.slice(1);
       const todoItem = {
-        taskName: inputTask,
+        taskName: capitalizedTask,
         id: uuidv4(),
         status: "todo",
       };
       setTodoList((prevList) => [todoItem, ...prevList]);
     }
   };
-
+  const deleteTodo = (taskId) => {
+    console.log("deleting todo", taskId);
+    setTodoList((prevList) =>
+      prevList.filter((todoItem) => todoItem.id !== taskId)
+    );
+  };
+  const updateTodo = (taskId) => {
+    console.log("updating todo", taskId);
+    setTodoList((prevList) =>
+      prevList.map((todoItem) => {
+        if (todoItem.id === taskId) {
+          let newStatus = todoItem.status === "todo" ? "completed" : "todo";
+          return { ...todoItem, status: newStatus };
+        }
+        return todoItem;
+      })
+    );
+  };
   return (
     <>
       <InputContainer addTodo={addTodo}></InputContainer>
       <div className="tabs-container">
         <Tabs
           defaultActiveKey="allTodos"
+          activeKey={activeTab}
+          onSelect={(key) => setActiveTab(key)}
           id="todo-nav"
           className="todo-nav"
           justify
         >
-          <Tab eventKey="allTodos" title="All Todods">
-            <AllTodos todoList={todoList} />
+          <Tab
+            eventKey="allTodos"
+            title={<span className="tab-title">All Todos</span>}
+          >
+            <AllTodos
+              todoList={todoList}
+              updateTodo={updateTodo}
+              deleteTodo={deleteTodo}
+            />
           </Tab>
-          <Tab eventKey="todos" title="Todos">
-            <Todos todoList={todoList} />
+          <Tab
+            eventKey="todos"
+            title={<span className="tab-title">Todos</span>}
+          >
+            <AllTodos
+              todoList={todoArray}
+              updateTodo={updateTodo}
+              deleteTodo={deleteTodo}
+            />
           </Tab>
-          <Tab eventKey="completed" title="Completed">
-            <CompletedTodos todoList={todoList} />
+          <Tab
+            eventKey="completed"
+            title={<span className="tab-title">Completed</span>}
+          >
+            <AllTodos
+              todoList={completedTodoList}
+              updateTodo={updateTodo}
+              deleteTodo={deleteTodo}
+            />
           </Tab>
         </Tabs>
       </div>
